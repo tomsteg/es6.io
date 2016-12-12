b1b81aca6a503aad50f17ca8c71893a3ad209cd8
Thomas Steglich <info@thomas-steglich.de>
2016-12-12 07:47:57 +0100

    solution adoption for exercise 1

--- playground.js
+++ playground.js
@@ -1,30 +1,12 @@
-const listItems = Array.from(document.getElementsByTagName('li'));
-const videoList = listItems.map(item => {
-	const video = {
-		time: item.getAttribute('data-time'),
-		title: item.innerHTML
-	};
-	return video;
-});
-const flexboxVideoList = videoList.filter(video => {
-	return video.title.match(/^Flexbox.*/) !== null;
-});
-const timeList = flexboxVideoList.map(video => video.time);
-let totalSeconds = 0;
-const secondsList = timeList.map(time => {
-	let timeSliced = time.split(':');
-	let minutes = parseInt(timeSliced[0], 10);
-	let seconds = parseInt(timeSliced[1], 10);
-	let timeInSeconds = minutes * 60 + seconds;
-	totalSeconds += timeInSeconds;
-	return timeInSeconds;
-});
-let hours = Math.floor(totalSeconds / 3600);
-totalSeconds %= 3600;
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60;
+const items = Array.from(document.querySelectorAll('[data-time]'));
+const filtered = items
+	.filter(item => item.textContent.includes('Flexbox'))
+	.map(item => item.dataset.time)
+	.map(timecode => {
+		const parts = timecode.split(':').map(part => parseFloat(part));
+		return (parts[0] * 60) + parts[1];
+	})
+	.reduce((runningTotal, seconds) => runningTotal + seconds);
 
-console.log(secondsList);
-console.log(totalSeconds);
-console.log(minutes + ':' + seconds);
+console.log(filtered);
 
