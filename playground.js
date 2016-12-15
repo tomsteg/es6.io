108738f07b72cfaebbc31928eafc0a2bccf47bff
Thomas Steglich <info@thomas-steglich.de>
2016-12-15 21:12:48 +0100

    template literals (part 2)

--- playground.js
+++ playground.js
@@ -26,3 +26,25 @@ ${song.featuring ? `(Featuring ${song.featuring})` : ''}
 `;
 
 document.querySelector('#song').innerHTML = markupSong;
+
+const beer = {
+	name: 'Belgian Wit',
+	brewery: 'Steam Whistle Brewery',
+	keywords: ['pale', 'cloudy', 'spiced', 'crisp']
+};
+
+function renderKeywords(keywords) {
+	return `
+	<ul>
+	${keywords.map(keyword => `<li>${keyword}</li>`).join('')}
+	</ul>
+		`;
+}
+
+const markupBeer = `
+<h2>${beer.name}</h2>
+<p class="brewery">${beer.brewery}</p>
+${renderKeywords(beer.keywords)}
+`;
+
+document.querySelector('#beer').innerHTML = markupBeer;
