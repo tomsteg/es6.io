7d25bf8797be17ef446a175b293804487f866dd9
Thomas Steglich <info@thomas-steglich.de>
2016-12-21 19:15:59 +0100

    tagged templates exercise 15

--- playground.js
+++ playground.js
@@ -1,50 +1,12 @@
-const dogs = [
-	{ name: 'Snickers', age: 2},
-	{ name: 'Hugo', age: 8},
-	{ name: 'Sunny', age: 1}
-];
-
-const markupDogs = `
-	<ul class="dogs">
-	${dogs.map(dog => `<li>${dog.name} is ${dog.age * 7}`).join('')}
-	</ul>
-`;
-
-document.querySelector('#dogs').innerHTML = markupDogs;
-
-const song = {
-	name: 'Dying to live',
-	artist: 'Tupac',
-	featuring: 'Biggie Smalls'
-};
-
-const markupSong = `
-<p>
-${song.name} - ${song.artist}
-${song.featuring ? `(Featuring ${song.featuring})` : ''}
-</p>
-`;
-
-document.querySelector('#song').innerHTML = markupSong;
-
-const beer = {
-	name: 'Belgian Wit',
-	brewery: 'Steam Whistle Brewery',
-	keywords: ['pale', 'cloudy', 'spiced', 'crisp']
-};
-
-function renderKeywords(keywords) {
-	return `
-	<ul>
-	${keywords.map(keyword => `<li>${keyword}</li>`).join('')}
-	</ul>
-		`;
+function highlight(strings, ...values) {
+	let str = '';
+	strings.forEach((string, i) => {
+		str += `${string} <span class='hl'>${values[i] || ''}</span>`;
+	});
+	return str;
 }
 
-const markupBeer = `
-<h2>${beer.name}</h2>
-<p class="brewery">${beer.brewery}</p>
-${renderKeywords(beer.keywords)}
-`;
-
-document.querySelector('#beer').innerHTML = markupBeer;
+const name = 'Snickers';
+const age = 100;
+const sentence = highlight`My dog's name is ${name} and he is ${age} years old.`;
+document.body.innerHTML = sentence;
