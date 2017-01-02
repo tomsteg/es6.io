a89aa94ffd02acfa8c5029d7de3edc53e29f1b41
tst <tst@agenda-software.de>
2017-01-02 13:32:05 +0100

    for of with objects

--- playground.js
+++ playground.js
@@ -1,34 +1,16 @@
-const cuts = ['Chuck', 'Brisket', 'Shank', 'Short Rib'];
+const apple = {
+	color: 'Red',
+	size: 'Meidum',
+	weight: 50,
+	sugar: 10
+};
 
-for (const [index, cut] of cuts.entries()) {
-	console.log(`${cut} is the ${index + 1} item`);
+for (const prop of Object.keys(apple)) {
+	const value = apple[prop];
+	console.log(value, prop);
 }
 
-function addUpNumbers() {
-	let total = 0;
-	// arguments is not a tru array
-	console.log(arguments);
-	for (const num of arguments) {
-		total += num;
-	}
-	console.log(total);
-	return total;
-}
-
-addUpNumbers(10,23,52,23,34,13,123);
-
-
-const name = 'Thomas Steglich';
-
-for (const char of name) {
-	console.log(char);
-}
-
-
-const ps = document.querySelectorAll('p');
-
-for (const paragraph of ps) {
-	paragraph.addEventListener('click', function() {
-		console.log(this.textContent);
-	});
+for (const prop in apple) {
+	const value = apple[prop];
+	console.log(value, prop);
 }
