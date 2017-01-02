212def0c7895f3e3bd24ff953f473f05dd12797c
tst <tst@agenda-software.de>
2017-01-02 13:58:14 +0100

    Array.from and Array.of

--- playground.js
+++ playground.js
@@ -1,16 +1,20 @@
-const apple = {
-	color: 'Red',
-	size: 'Meidum',
-	weight: 50,
-	sugar: 10
-};
+const people = document.querySelectorAll('.people p');
 
-for (const prop of Object.keys(apple)) {
-	const value = apple[prop];
-	console.log(value, prop);
+// people ist eine nodeList und kein Array
+
+const names = Array.from(people).map(person => person.textContent);
+console.log(names);
+
+const moreNames = Array.from(people, person => person.textContent);
+console.log(moreNames);
+
+function sumAll() {
+	return Array.from(arguments).reduce((prev, next) => prev + next, 0);
 }
 
-for (const prop in apple) {
-	const value = apple[prop];
-	console.log(value, prop);
-}
+const sum = sumAll(2,45,56,34,65,188,12,41);
+
+console.log(sum);
+
+const ages = Array.of(12,14,6);
+console.log(ages);
