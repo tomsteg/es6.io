fbae3479270e59dc0436db394bb78ca917fdcaac
Thomas Steglich <info@thomas-steglich.de>
2017-01-15 12:13:28 +0100

    more spread examples

--- playground.js
+++ playground.js
@@ -1,7 +1,26 @@
-const heading = document.querySelector('.jump');
+const people = Array.from(document.querySelectorAll('.people p'));
+const names = people.map(person => person.textContent);
 
-function sparanWrap(word) {
-	return [...word].map(letter => `<span>${letter}</span>`).join('');
-}
+const deepDish = {
+	pizzaName: 'Deep Dish',
+	size: 'Medium',
+	ingredients: ['Bacon', 'Tomatos', 'Mozarella', 'Onions']
+};
 
-heading.innerHTML = sparanWrap(heading.textContent);
+const shoppingList = ['Milk', 'Flour', ...deepDish.ingredients];
+
+//console.log(shoppingList);
+
+const comments = [
+	{ id: 209384, text: 'I love your dog!' },
+	{ id: 523423, text: 'Cuuute! 🐐' },
+	{ id: 632429, text: 'You are so dumb' },
+	{ id: 192834, text: 'Nice work on this wes!' },
+];
+
+const id = 632429;
+
+const commentIndex = comments.findIndex(comment => comment.id === id);
+const newComments = [...comments.slice(0, commentIndex), ...comments.slice(commentIndex + 1)];
+
+console.log(newComments);
