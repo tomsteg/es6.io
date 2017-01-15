62a1b729a62a7edbd5d6f98daa0a4f5bc361ba95
Thomas Steglich <info@thomas-steglich.de>
2017-01-15 12:20:42 +0100

    spread into a function

--- playground.js
+++ playground.js
@@ -1,26 +1,6 @@
-const people = Array.from(document.querySelectorAll('.people p'));
-const names = people.map(person => person.textContent);
+const inventors = ['Einstein', 'Newton', 'Galileo'];
+const newInventors = ['Musk', 'Jobs'];
 
-const deepDish = {
-	pizzaName: 'Deep Dish',
-	size: 'Medium',
-	ingredients: ['Bacon', 'Tomatos', 'Mozarella', 'Onions']
-};
+inventors.push(...newInventors);
 
-const shoppingList = ['Milk', 'Flour', ...deepDish.ingredients];
 
-//console.log(shoppingList);
-
-const comments = [
-	{ id: 209384, text: 'I love your dog!' },
-	{ id: 523423, text: 'Cuuute! 🐐' },
-	{ id: 632429, text: 'You are so dumb' },
-	{ id: 192834, text: 'Nice work on this wes!' },
-];
-
-const id = 632429;
-
-const commentIndex = comments.findIndex(comment => comment.id === id);
-const newComments = [...comments.slice(0, commentIndex), ...comments.slice(commentIndex + 1)];
-
-console.log(newComments);
