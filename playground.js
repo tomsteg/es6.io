134c10c1613b5bdcab99ebfa41fb0f91b655432a
Thomas Steglich <info@thomas-steglich.de>
2017-01-20 19:29:26 +0100

    working with multiple promises

--- playground.js
+++ playground.js
@@ -1,6 +1,11 @@
-const inventors = ['Einstein', 'Newton', 'Galileo'];
-const newInventors = ['Musk', 'Jobs'];
+const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
+const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
 
-inventors.push(...newInventors);
-
-
+Promise
+	.all([postsPromise, streetCarsPromise])
+	.then(responses => {
+		return Promise.all(responses.map(res => res.json()));
+	})
+	.then(responses => {
+		console.log(responses);
+	});
